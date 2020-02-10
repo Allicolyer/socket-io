f2a07ec85dcb186cb4e654d70de81441da447d5a
Alli Colyer <allicolyer@gmail.com>
2020-02-10 17:08:35 -0500

    change color of current user's messages

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
 
 io.on("connection", function(socket) {
   socket.on("chat message", function(msg) {
-    io.emit("chat message", msg);
+    socket.broadcast.emit("chat message", msg);
   });
   console.log("a user connected");
   socket.on("disconnect", function() {
