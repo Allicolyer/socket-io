51b690502ad4284c3b5ec368d43fd5ab61b58d9c
Alli Colyer <allicolyer@gmail.com>
2020-02-11 15:46:29 -0500

    refactor javascript functions, add outgoing sound, add .gitignore, remove node modules and package-lock

--- public/main.js
+++ public/main.js
@@ -3,25 +3,42 @@ $(function() {
 
   $("form").submit(function(e) {
     e.preventDefault(); // prevents page reloading
+
+    //get name and message from input fields
     let name = $("#name").val();
-    let message = $("#m").val();
-    let html =
-      "<li class='current-user'><span class='name'>" +
-      name +
-      ": </span>" +
-      message +
-      "</li>";
+    let message = $("#message").val();
+    //emit the message to all other users
     socket.emit("chat message", name, message);
-    $("#messages").append(html);
-    $("#m").val("");
+    ///add the message to the window
+    addMessage(name, message, true);
+    //play outgoing message sound
+    playAudio("outgoing-message.wav");
+    //clear the input field
+    $("#message").val("");
     return false;
   });
-  socket.on("chat message", function(name, message) {
-    let html =
-      "<li><span class='name'>" + name + ": </span>" + message + "</li>";
-    $("#messages").append(html);
 
-    let audio = new Audio('incoming-message.mp3');
-    audio.play();
+  socket.on("chat message", function(name, message) {
+    ///add the message to the window
+    addMessage(name, message, false);
+    //play incoming message sound
+    playAudio("incoming-message.mp3");
   });
 });
+
+//play the audio file
+const playAudio = file => {
+  let audio = new Audio(file);
+  audio.play();
+};
+
+const addMessage = (name, message, currentUser) => {
+  //set up name and message span
+  let nameAndMessage = `<span class='name'>${name}:</span> ${message}`;
+  //conditionally add the current-user class
+  let html =
+    `<li ${(currentUser && "class=current-user") || ""}>` +
+    nameAndMessage +
+    `</li>`;
+  $("#messages").append(html);
+};
