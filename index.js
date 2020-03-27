c01093d5c6f331eae77ea83abd41b891079bddc9
Alli Colyer <allicolyer@gmail.com>
2020-03-27 15:53:54 -0400

    fix buggy socket code

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var transcripts = {
 
 io.on("connection", function(socket) {
   socket.join("Misc");
+  socket.emit("transcript", transcripts["Misc"]);
   socket.on("chat message", function(name, msg, room) {
     console.log("receiving message from " + name);
     //sends the message to everyone else
@@ -32,14 +33,14 @@ io.on("connection", function(socket) {
     socket.emit("chat message", name, msg, true, false);
     //update the transcript
     transcripts[room] = `${transcripts[room]} ${msg}`;
-    socket.emit("transcript", transcripts[room]);
+    io.sockets.emit("transcript", transcripts[room]);
   });
   socket.on("AI message", function(name, msg, room) {
     //update transcript??
     io.sockets.in(room).emit("chat message", name, msg, false, true);
     //update the transcript
     transcripts[room] = `${transcripts[room]} ${msg}`;
-    socket.emit("transcript", transcripts[room]);
+    io.sockets.emit("transcript", transcripts[room]);
   });
   console.log("a great user connected");
   socket.on("disconnect", function() {
